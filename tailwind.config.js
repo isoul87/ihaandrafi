41a1afb09f9ddf5407bac75b1141c67a4d6cad83
isoul87 <atbrxnk@gmail.com>
2025-09-27 15:15:23 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+  /** @type {import('tailwindcss').Config} */
+ export default {
+   content: ["./index.html"],
+    theme: {
+      extend: {
+        fontFamily:{
+          'script': ['Alex Brush', 'cursive'],
+          'cinzel': ['Cinzel', 'cursive'],
+          'cross': ['Croissant One', 'cursive'],
+        },
+      },
+    },
+    plugins: [],
+  }
